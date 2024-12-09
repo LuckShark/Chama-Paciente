d85ed5ae612f7196e60192f0d98a2c1c4cd119a2
lucasaraujoalmeida <87248601+LuckShark@users.noreply.github.com>
2024-12-09 13:52:47 -0300

    first

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { TesteComponent } from './teste/teste.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component:TesteComponent},
+    {path: '**', redirectTo:''}
+];
---
+++ src/app/teste/teste.component.ts
@@ -0,0 +1,63 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog'; 
+import { ModalComponent } from '../modal/modal.component'; 
+@Component({
+  selector: 'app-teste',
+  standalone: true,
+  imports: [
+    CommonModule,
+    FormsModule,
+    MatDialogModule, // Import do módulo do MatDialog
+  ],
+  templateUrl: './teste.component.html',
+  styleUrls: ['./teste.component.scss'],
+})
+export class TesteComponent {
+  patientName: string = '';
+  consultationRoom: string = '';
+  lastCall: { patientName: string; consultationRoom: string } | null = null;
+  previousCalls: { patientName: string; consultationRoom: string }[] = [];
+
+  constructor(private dialog: MatDialog) {} // Injeção do MatDialog
+
+  addCall() {
+    if (this.patientName && this.consultationRoom) {
+      if (this.lastCall) {
+        this.previousCalls.unshift(this.lastCall);
+        if (this.previousCalls.length > 3) {
+          this.previousCalls.pop();
+        }
+      }
+
+      this.lastCall = {
+        patientName: this.patientName,
+        consultationRoom: this.consultationRoom,
+      };
+
+      // Abrir o modal
+      this.dialog.open(ModalComponent, {
+        data: {
+          patientName: this.lastCall.patientName,
+          consultationRoom: this.lastCall.consultationRoom,
+        },
+        width: '80%',
+        height: '80%',
+      });
+
+      // Fala o nome do paciente
+      this.readAloud(`${this.patientName} - ${this.consultationRoom}`);
+
+      // Limpar os campos
+      this.patientName = '';
+      this.consultationRoom = '';
+    }
+  }
+
+  readAloud(text: string) {
+    const speech = new SpeechSynthesisUtterance(text);
+    speech.lang = 'pt-BR';
+    speechSynthesis.speak(speech);
+  }
+}
