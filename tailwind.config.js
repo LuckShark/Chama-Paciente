653b682962a3682fe2f7fbc59a3bb2903ea1f636
lucasaraujoalmeida <87248601+LuckShark@users.noreply.github.com>
2024-12-09 14:50:12 -0300

    ux added

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,22 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        green: {
+          50: '#e6f7e4',
+          100: '#c4ebc3',
+          200: '#9fdd9d',
+          300: '#7acf76',
+          400: '#5dca56',
+          500: '#1ecc0e',
+          600: '#1bb20d',
+          700: '#18960b',
+          800: '#1c7c06',
+          900: '#135805',
+        },
+      },
+    },
   },
   plugins: [],
 }
